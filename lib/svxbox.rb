14cb0a0c6a19b3a9ac43f461d150b971b2d535fd
Docunext <albert.lash@docunext.com>
2010-03-30 21:56:26 -0400

    mongobongo

--- lib/svxbox.rb
+++ lib/svxbox.rb
@@ -4,6 +4,7 @@ require File.dirname(__FILE__) + '/svxbox/markupguppy'
 
 # Not included by default
 require File.dirname(__FILE__) + '/svxbox/sinatricus'
+require File.dirname(__FILE__) + '/svxbox/mongobongo'
 
 # Must come last
 require File.dirname(__FILE__) + '/svxbox/supabali'
