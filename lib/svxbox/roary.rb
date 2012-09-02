a1350fd0b5a84923c19814e514f0605c2a4ce9e4
docunext <albert.lash@docunext.com>
2012-09-02 02:59:52 +0000

    wrong syntax for self.class

--- lib/svxbox/roary.rb
+++ lib/svxbox/roary.rb
@@ -2,24 +2,34 @@ module SvxBox::Roary
   extend ActiveSupport::Concern
 
   included do
-    cattr_reader :param_suffix
+    cattr_accessor :param_suffix
     alias_method_chain :to_param, :suffix
   end
 
   def to_param_with_suffix
+    # The aliased to_param method
     if param_suffix && self.respond_to?(param_suffix)
-      suffix = send(param_suffix).parameterize
+      suffix = self.send(param_suffix).parameterize
       return "#{to_param_without_suffix}-#{suffix}"
     else
       return to_param_without_suffix  
     end
   end
 
+  #   class Something < ActiveRecord::Base
+  #     include SvxBox::Roary
+  #     to_param_suffix :name
+  #
+  #   end
   module ClassMethods
-    # Set the param_suffix class attribute
     def to_param_suffix(m)
+      # Set the param_suffix class attribute with this method
       raise ArgumentError, "Need a symbol." unless m.is_a? Symbol 
-      @@param_suffix = m
+      self.param_suffix = m
+    end
+    
+    def per_page
+      10
     end
   end
 end
